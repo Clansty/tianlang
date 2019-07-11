4ebb8807e7d59e1b58daca42886fbce77020f1a7
凌莞 <i@gao4.pw>
2019-07-11 19:31:22 +0800

    发送xml 测试 *龙门粗口*

---
+++ QYPlugin/SendTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clansty.tianlang
+{
+    public partial class SendTest : Form
+    {
+        public SendTest()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            S.Test(textBox1.Text);
+        }
+    }
+}
