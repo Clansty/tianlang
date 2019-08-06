44530374d7f0f5405ccb90667024b3f25c1adf5c
凌莞 <i@gao4.pw>
2019-08-06 21:33:26 +0800

    啊噗噜派

--- QYPlugin/莱茵生命/Common.cs
+++ QYPlugin/莱茵生命/Common.cs
@@ -10,7 +10,7 @@ namespace Clansty.tianlang
 {
     public static class C
     {
-        public const string Version = "3.0.10.3";
+        public const string Version = "3.0.10.4";
 
         public static void Write(string text, ConsoleColor color = ConsoleColor.White)
         {
