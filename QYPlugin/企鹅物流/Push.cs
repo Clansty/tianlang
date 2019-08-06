8eb22ca96d056137fa7f035af1c6baf3bb9c316d
凌莞 <i@gao4.pw>
2019-08-06 17:28:00 +0800

    又一推送号被封

--- QYPlugin/企鹅物流/Push.cs
+++ QYPlugin/企鹅物流/Push.cs
@@ -29,7 +29,7 @@ namespace Clansty.tianlang
                 {
                     if (i.Card.IndexOf(ftxt) > -1)
                     {
-                        Robot.Send.QY_sendGroupTmpMsg(Robot.AuthCode, 2125742312, long.Parse(G.major), long.Parse(i.QQ), pmsg);
+                        Robot.Send.QY_sendGroupTmpMsg(Robot.AuthCode, 168375232, long.Parse(G.major), long.Parse(i.QQ), pmsg);
                         C.WriteLn($"{i.Card} <-", ConsoleColor.Magenta);
                         Thread.Sleep(15000);
                     }
--- QYPlugin/罗德岛/Robot.cs
+++ QYPlugin/罗德岛/Robot.cs
@@ -340,7 +340,7 @@ namespace Clansty.tianlang
                 if (!isok)
                     return false;
                 //XXX: 要封也是测试服
-                return QY_setModifyGroupMemberCard(AuthCode, 2125742312, t, m, name) == 0;
+                return QY_setModifyGroupMemberCard(AuthCode, 168375232, t, m, name) == 0;
             }
             [DllImport("QYOffer.dll")]
             private static extern int QY_setModifyGroupMemberCard(int authCode, long qqID, long targ, long s, string a);
