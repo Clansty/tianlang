10892917f2c68091bd4f55e4b11c1dbb8a571138
凌莞 <i@gao4.pw>
2019-08-06 18:40:42 +0800

    龙门近卫局时刻关注安全情况

---
+++ QYPlugin/陈的办公室/RealNameVerify.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clansty.tianlang.陈的办公室
+{
+    /// <summary>
+    /// 龙门近卫局时刻关注安全情况
+    /// </summary>
+    static class RealNameVerify
+    {
+    }
+}
