deda51f9afbdfd82d746b0774141c8e7c169eddd
凌莞 <i@gao4.pw>
2019-08-06 21:32:59 +0800

    呼

--- QYPlugin/喀兰贸易/SetupV3.cs
+++ QYPlugin/喀兰贸易/SetupV3.cs
@@ -65,29 +65,37 @@ namespace Clansty.tianlang
                     u.Nick = m;
                     u.Namecard = u.ProperNamecard;
                     u.Step = 3;
+                    if (u.Name != "")
+                    {
+                        FinishWizard();
+                    }
                     C.WriteLn($"{u.Uin} 加群向导进入第 3 步", ConsoleColor.Cyan);
-                    e.Reply(Strs.Get("setupStep3"));//请告诉我你的姓名，这并不会在群里公开
+                    e.Reply(Strs.Get("setupStep3"));//请告诉我你的姓名，这并不会在群里公开               
                     return;
                 case 3:
                     u.Name = m;
-                    u.Step = -1;
-                    u.Status = Status.no;
-                    C.WriteLn($"{u.Uin} 加群向导完成", ConsoleColor.Green);
-                    e.Reply(Strs.Get("setupSetNC", u.ProperNamecard));//你的群名片已修改为 {0}
-                    e.Reply(Strs.Get("setupOK"));//目前我们需要的信息就这么多，祝你在群里玩的开心
-                    string tip = Strs.Get($"setupTip{u.Enrollment}");//根据年级来分的提示
-                    if (tip != "")
-                        e.Reply(tip); //你是高一的，建议同时加入 2018 级新高一年级群 //??2019级新生即将来临?敬请期待??? 
-                    tip = Strs.Get($"setupTipAll");//给所有人的广告位
-                    if (tip != "")
-                        e.Reply(tip); //目前是这个：【西花园事务所】是江苏省苏州第十中学校学生自建生活服务平台，添加【西花园事务所】为特别关心，可以第一时间收到最新消息 
-                    S.Si(u.ToXml("新人信息"));
+                    FinishWizard();
                     return;
                 default:
                     HandleStepErr();
                     return;
             }
 
+            void FinishWizard()
+            {
+                u.Step = -1;
+                u.Status = Status.no;
+                C.WriteLn($"{u.Uin} 加群向导完成", ConsoleColor.Green);
+                e.Reply(Strs.Get("setupSetNC", u.ProperNamecard));//你的群名片已修改为 {0}
+                e.Reply(Strs.Get("setupOK"));//目前我们需要的信息就这么多，祝你在群里玩的开心
+                string tip = Strs.Get($"setupTip{u.Enrollment}");//根据年级来分的提示
+                if (tip != "")
+                    e.Reply(tip); //你是高一的，建议同时加入 2018 级新高一年级群 //??2019级新生即将来临?敬请期待??? 
+                tip = Strs.Get($"setupTipAll");//给所有人的广告位
+                if (tip != "")
+                    e.Reply(tip); //目前是这个：【西花园事务所】是江苏省苏州第十中学校学生自建生活服务平台，添加【西花园事务所】为特别关心，可以第一时间收到最新消息 
+                S.Si(u.ToXml("新人信息"));
+            }
             void HandleStepErr()
             {
                 C.WriteLn($"{e.FromQQ} setup 出现 step 有毛病的问题，step = {u.Step}，已重置状态", ConsoleColor.Magenta);
@@ -98,7 +106,7 @@ namespace Clansty.tianlang
 
         public static void New(string qq, bool first = false)
         {
-            new User(qq)
+            var u = new User(qq)
             {
                 Status = Status.setup,
                 Step = 1
@@ -110,7 +118,14 @@ namespace Clansty.tianlang
                 S.Private(qq, Strs.Get("setupGreetingMist")); //如果输入错误，你可以说<上一步>，或者<重新开始>.(不包括尖括号哦）
                 S.Major(Strs.Get("setupGroupWelcome", qq)); //欢迎新人 [@{0}]，请注意我给你发送的私聊消息哦~
             }
-            S.Private(qq, Strs.Get("setupStep1")); //请回复你开学后的年级或毕业的年份，金阊分部的同学请在前面加上金阊\n例如 高一, 初三, 2019届, 2019届初中, 金阊高一
+            // TODO: 这里根据已有信息判断需要进入哪一步
+            if (u.Enrollment < 1970)
+                S.Private(qq, Strs.Get("setupStep1")); //请回复你开学后的年级或毕业的年份，金阊分部的同学请在前面加上金阊\n例如 高一, 初三, 2019届, 2019届初中, 金阊高一
+            else
+            {
+                u.Step = 2;
+                S.Private(qq, Strs.Get("setupStep2"));
+            }
             C.WriteLn($"{qq} 加群向导启动 first = {first}", ConsoleColor.Cyan);
         }
     }
--- QYPlugin/莱茵生命/QYEvents.cs
+++ QYPlugin/莱茵生命/QYEvents.cs
@@ -88,12 +88,97 @@ namespace Clansty.tianlang
             if (e.Group == G.major)
             {
                 User u = new User(e.FromQQ);
+                C.Write("有人 ", ConsoleColor.DarkCyan);
+                C.Write(e.FromQQ, ConsoleColor.Cyan);
+                C.Write(" 加群 ", ConsoleColor.DarkCyan);
+                C.Write(e.Msg, ConsoleColor.Cyan);
+                C.WriteLn("");
                 if (u.Role == UserType.blackListed)
                 {
                     e.Reject("blacklisted");
                     S.Si(u.ToXml("加群申请已拒绝: 黑名单用户"));
                     return;
                 }
+                // TODO: 实名验证审核
+                if (e.Msg.IndexOf(" ") < 0)
+                {
+                    e.Reject("请按正确格式填写");
+                    S.Si(u.ToXml("加群申请已拒绝: 格式错误"));
+                    return;
+                }
+
+                var enr = UserInfo.ParseEnrollment(e.Msg.GetLeft(" "));
+                var name = e.Msg.GetRight(" ").Trim();
+
+                if (u.Enrollment > 1970 && enr != u.Enrollment)
+                {
+                    e.Reject("年级与登记的不匹配，请联系管理员");
+                    S.Si(u.ToXml("加群申请已拒绝: 年级与登记的不匹配") + $"\n申请信息: {e.Msg}");
+                    return;
+                }
+
+                u.Enrollment = enr;
+
+                if (enr != 2017 && enr != 2018)
+                {                    
+                    u.Name = name;
+                    u.Junior = UserInfo.ParseJunior(e.Msg.GetLeft(" "));
+                    S.Si(u.ToXml("非高二高三请手动审核"));
+                    return;
+                }
+
+                if (u.Verified)
+                {
+                    if (u.Name != name)
+                    {
+                        e.Reject("已实名账户请用登记姓名加入，如有问题请联系管理员");
+                        S.Si(u.ToXml("加群申请已拒绝: 已实名账户尝试 override") + $"\n申请信息: {e.Msg}");
+                        return;
+                    }
+                    e.Accept();
+                    S.Si(u.ToXml("加群申请已同意: 已实名用户"));
+                    return;
+                }
+
+                var chk = RealName.Check(name);
+
+                if(chk.Status == RealNameStatus.notFound)
+                {
+                    e.Reject("查无此人");
+                    S.Si(u.ToXml("加群申请已拒绝: 查无此人") + $"\n申请信息: {e.Msg}");
+                    return;
+                }
+                
+                if(chk.OccupiedQQ!=null && chk.OccupiedQQ != e.FromQQ)
+                {
+                    e.Reject("此身份已有一个账户加入，如有疑问请联系管理员");
+                    S.Si(u.ToXml("加群申请已拒绝: 此人已存在") + $"\n申请信息: {e.Msg}");
+                    return;
+                }
+
+                if (chk.OccupiedQQ == null)
+                {
+                    //尝试进行验证
+                    u.Name = name;
+                    u.Junior = UserInfo.ParseJunior(e.Msg.GetLeft(" "));
+                    if (u.Verified)
+                    {
+                        e.Accept();
+                        S.Si(u.ToXml("加群申请已同意: 实名认证成功"));
+                        return;
+                    }
+                    var err = u.VerifyMsg;
+                    if(err== RealNameVerifingResult.unmatch)
+                    {
+                        e.Reject("姓名与年级不匹配，请检查");
+                        S.Si(u.ToXml("加群申请已拒绝: 姓名与年级不匹配") + $"\n申请信息: {e.Msg}");
+                        return;
+                    }
+                    e.Reject("玄学错误，请联系管理员");
+                    S.Si(u.ToXml("加群申请已拒绝: 玄学错误，此错误不应该由本段代码处理") + $"\n申请信息: {e.Msg}\n未预期的错误: {err}");
+                    return;
+                }
+
                 if (Rds.SContains("knownUsers", e.FromQQ))
                 {
                     e.Accept();
