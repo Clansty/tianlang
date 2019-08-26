8da53f7fc80d9d1f5a5d3b068aa143845e8b46a6
凌莞 <i@gao4.pw>
2019-08-26 20:09:07 +0800

    取消图标

--- QYPlugin/企鹅物流/Push.Designer.cs
+++ QYPlugin/企鹅物流/Push.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Push));
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -81,7 +80,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Push";
             this.Text = "企鹅物流";
             this.ResumeLayout(false);
