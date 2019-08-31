fb7aa581290c4961a552d2c7a217b850a024f3cb
凌莞 <i@gao4.pw>
2019-08-31 19:40:33 +0800

    旧人逻辑微调

--- QYPlugin/莱茵生命/QYEvents.cs
+++ QYPlugin/莱茵生命/QYEvents.cs
@@ -305,9 +305,15 @@ namespace Clansty.tianlang
                 {
                     u.Namecard = u.ProperNamecard;
                     u.Status = Status.no;
-                    S.Private(member, u.ToXml("欢迎回来"));
-                    S.Private(member, Strs.Get("welcomeBack"));//"如果以上信息有误，你可以回复 <setup> 来重新设置"
-                    S.Si(u.ToXml("来了个旧人"));
+                    string tip = Strs.Get($"setupTip{u.Enrollment}");//根据年级来分的提示
+                    if (tip != "")
+                        S.Private(member, tip); //你是高一的，建议同时加入 2018 级新高一年级群 //??2019级新生即将来临?敬请期待??? 
+                    tip = Strs.Get($"setupTipAll");//给所有人的广告位
+                    if (tip != "")
+                        S.Private(member, tip); //目前是这个：【西花园事务所】是江苏省苏州第十中学校学生自建生活服务平台，添加【西花园事务所】为特别关心，可以第一时间收到最新消息 
+                    //S.Private(member, u.ToXml("欢迎回来"));
+                    //S.Private(member, Strs.Get("welcomeBack"));//"如果以上信息有误，你可以回复 <setup> 来重新设置"
+                    S.Si(u.ToXml("数据库存在此人信息，新人向导跳过"));
                     return;
                 }
             }
