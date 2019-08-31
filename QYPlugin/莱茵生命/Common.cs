1f8cdd49c9de05ab6df391518385af1a8f8effa5
凌莞 <i@gao4.pw>
2019-08-31 14:11:40 +0800

    推进大版本号

--- QYPlugin/莱茵生命/Common.cs
+++ QYPlugin/莱茵生命/Common.cs
@@ -10,7 +10,7 @@ namespace Clansty.tianlang
 {
     public static class C
     {
-        public const string Version = "3.0.12.3";
+        public const string Version = "3.0.13.0";
 
         public static void Write(string text, ConsoleColor color = ConsoleColor.White)
         {
