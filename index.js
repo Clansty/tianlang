e747faabccfc05c83513279809998be36afc812c
root <root@localhost.localdomain>
2020-07-13 22:05:35 +0800

    upload

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const groups = require("./groups")
 module.exports = (app) => {
     //var logger = app.logger()
     //logger.info("app starting in " + (global.dev ? "dev" : "production") + " mode...");
-    //app.group(groups.major).prependMiddleware(require('./middlewares/test'))
-    app.receiver.on('group-increase', require('./on-group-increase'))
+    //app.groups.middleware(require('./middlewares/test'))
+    // app.receiver.on('group-increase', require('./on-group-increase'))
+    // app.receiver.on('ready', aaa)
+
+
+    // 手动添加要获取的字段，下面会介绍
+
+    app.command('echo <message>')
+        .action(({ meta }, message) => meta.$send(message))
 }
--- middlewares/test.js
+++ middlewares/test.js
@@ -1,4 +1,4 @@
 module.exports=(meta, next)=>{
     console.log(meta);
-
+    console.log(meta.$user);
 }
