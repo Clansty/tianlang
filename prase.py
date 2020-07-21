0e889a04c94e9ef5d9eb0acf06bff821bcc4826c
lw <i@gao4.pw>
2020-07-21 14:30:52 +0800

    prase enr

---
+++ prase.py
@@ -0,0 +1,43 @@
+def enrollment(s: str) -> int:
+    s = s.strip()
+    if s == None or s == "":
+        return 0
+    # 去除多彩群名片标记
+    if s.startswith('<') and '>' in s:
+        s = s.partition('>')[2].strip()  # 文本取右边
+    s = s.replace("丨", " | ") #gun
+    s = s.replace("｜", " | ") #全角
+    if '|' in s:
+        m=enrollment(s.partition('|')[0].strip())
+        # 就是，有|的时候处理|左边的，不行再整个处理
+        if m>0:
+            return m
+    #左边无效或无左边
+    #!年级中文对应 enr 在这里
+    if '高一' in s or '高1' in s:
+        return 2020
+    if '高二' in s or '高2' in s:
+        return 2019
+    if '高三' in s or '高3' in s:
+        return 2018
+    if '级' in s:
+        # 有级先考虑，不然就是届
+        s=s.partition('级')[0].strip()
+        s=''.join(list(filter(str.isnumeric, s))) # 只取数字
+        s=int(s)
+        if 0<s<100:
+            s+=2000
+        if s>2000:
+            return s
+        return 0
+    if '届' in s:
+        s=s.partition('届')[0].strip()
+    s=''.join(list(filter(str.isnumeric, s))) # 只取数字
+    if not s:
+        return 0
+    s=int(s)
+    if 0<s<100:
+        s+=2000
+    if s>2000:
+        return s-3
+    return 0
--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-from user import User
-from realname import check
+import prase
 
-print(User(1).__repr__())
-
+print(prase.enrollment("2020届 | 丸"))
+print(prase.enrollment("2020届丨丸"))
+print(prase.enrollment("2020届学姐 | 丸"))
+print(prase.enrollment("20届 | 丸"))
+print(prase.enrollment("17级 | 丸"))
+print(prase.enrollment("2017级 | 丸")) #2017
+print(prase.enrollment("高三 | 丸"))
+print(prase.enrollment("18级 | 丸"))
+print(prase.enrollment("高三 丸"))
+print(prase.enrollment("2020级7班张三"))
+print(prase.enrollment("2020级新生 张三"))
+print(prase.enrollment("2016届2016班张三"))
+print(prase.enrollment("fffff"))
+print(prase.enrollment("高一/2020届初中"))
+print(prase.enrollment("2019届初中"))
