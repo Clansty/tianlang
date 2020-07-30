c1013dcffbcae9535a7061d8fe3de332bf208c79
凌莞 <i@gao4.pw>
2020-07-30 22:11:19 +0800

    跑起来啦

---
+++ NthsBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using Ac682.Hyperai.Clients.Mirai;
+using Hyperai.Serialization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System;
+
+namespace NthsBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new MiraiClientOptions()
+            {
+                AuthKey = "QwQQAQOwO",
+                Port = 8080,
+                Host = "localhost",
+                SelfQQ = 2981882373
+            };
+            var loggerFactory = LoggerFactory.Create(builder => {
+                builder.AddConsole();
+            }); 
+            var client = new MiraiClient(options, loggerFactory.CreateLogger<MiraiClient>(), new HyperCodeFormatter());
+            client.Connect();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                //TODO 输入处理
+            }
+        }
+    }
+}
