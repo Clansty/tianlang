07bedefe3b7bd52346d95912390de0b251806aca
凌莞 <i@gao4.pw>
2020-07-30 21:04:33 +0800

    QY, over

--- QYPlugin/莱茵生命/Common.cs
+++ QYPlugin/莱茵生命/Common.cs
@@ -10,7 +10,14 @@ namespace Clansty.tianlang
 {
     public static class C
     {
-        public const string Version = "3.0.17.4";//updated 20200728
+        /*
+         * TODO:
+         * 新生群名片标准化
+         * 检测是否成员来决定是否推荐大群/新生群
+         * 新生群后台实名实现
+         */
+        
+        public const string Version = "3.0.17.5";//20200730
 
         public static void Write(string text, ConsoleColor color = ConsoleColor.White)
         {
