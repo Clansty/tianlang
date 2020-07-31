49407098bc0fbd7b78958aa061258973960d1b48
凌莞 <i@gao4.pw>
2020-07-31 22:47:28 +0800

    今日 TODO 總結

--- Clansty.tianlang/Common.cs
+++ Clansty.tianlang/Common.cs
@@ -17,6 +17,14 @@ namespace Clansty.tianlang
          * 新生群名片标准化
          * 检测是否成员来决定是否推荐大群/新生群
          * 新生群后台实名实现
+         * User.Verified 的定義：至少得有名字吧
+         *
+         * 7/31 管理群會話 TODO 總結：
+         * 執行命令的時候 trim 掉回車
+         * 草了现在 branch 是 get only 的也得改（費點功夫）
+         * 申請加群裏面的名字也校驗
+         * 2020届初中是实名信息有的，改不了新的 enrollment
+         * 到时候会有一个人两个实名身份的情况（難度大）
          */
         
         public const string Version = "3.1.17.6";//20200731
