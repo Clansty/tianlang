cd98793cde0de1b24bc6e6b2f372876f7d092dd0
凌莞 <i@gao4.pw>
2020-08-04 14:16:23 +0800

    应用信息处理

--- LemonApp/AppInfo.cs
+++ LemonApp/AppInfo.cs
@@ -9,43 +9,43 @@ namespace LemonApp
     /// <summary>
     /// 请在这里填写应用的信息
     /// </summary>
-    static class AppInfo
+    class AppInfo
     {
         /// <summary>
         /// 固定 不要改
         /// </summary>
-        public const int apiVer = 6;
+        public readonly int apiVer = 6;
         /// <summary>
         /// 应用ID  1.尽量唯一  2.编译出来需要以 .lq.dll 结尾 方便框架识别
         /// </summary>
-        public const string appId = "com.lwqwq.lemon.sdk";
+        public readonly string appId = "com.lwqwq.lemon.sdk";
         /// <summary>
         /// 应用名称
         /// </summary>
-        public const string name = "柠檬乐园样例应用";
+        public readonly string name = "柠檬乐园样例应用";
         /// <summary>
         /// 应用版本
         /// </summary>
-        public const string ver = "1.0";
+        public readonly string ver = "1.0";
         /// <summary>
         /// 开发者ID ‘预留  随便填
         /// </summary>
-        public const string authkey = "123456";
+        public readonly string authkey = "123456";
         /// <summary>
         /// 作者名字
         /// </summary>
-        public const string author = "凌莞";
+        public readonly string author = "凌莞";
         /// <summary>
         /// 应用简介
         /// </summary>
-        public const string description = "样例应用简介";
+        public readonly string description = "样例应用简介";
         /// <summary>
         /// 应用详情显示的 URL
         /// </summary>
-        public const string url = "https://github.com/Clansty/LemonApp";
+        public readonly string url = "https://github.com/Clansty/LemonApp";
         /// <summary>
         /// 0 表示不需要取 cookies，需要请改为 1
         /// </summary>
-        public const int ck = 0;
+        public readonly int ck = 0;
     }
 }
--- LemonApp/Lemon/Robot.cs
+++ LemonApp/Lemon/Robot.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace LemonApp
 {
     static class Robot
     {
+        static int ac = 0;
+        [DllExport(CallingConvention.StdCall)]
+        static string AppInfo() => new JavaScriptSerializer().Serialize(new AppInfo());
+        [DllExport(CallingConvention.StdCall)]
+        static int Initialize(int acc)
+        {
+            ac = acc;
+            return 0;
+        }
 
     }
 }
