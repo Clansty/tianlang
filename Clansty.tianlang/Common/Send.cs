1eb815449c6436e6cd1619c65c69a0efb2933771
凌莞 <i@gao4.pw>
2020-09-09 15:03:45 +0800

    增加 tg 发送方法

--- Clansty.tianlang/Common/Send.cs
+++ Clansty.tianlang/Common/Send.cs
@@ -15,5 +15,21 @@
         internal static void Major(string msg) => Group(G.major, msg);
         internal static void Si(string msg) => Group(G.si, msg);
         internal static void Test(string msg) => Group(G.test, msg);
+        internal static void IDE(string msg) => Group(G.iDE, msg);
+
+        /// <summary>
+        /// 少用这个
+        /// </summary>
+        internal static class TG
+        {
+            internal static void Text(long id, string msg)
+            {
+                C.TG.SendTextMessageAsync(id, msg);
+            }
+            internal static void Major(string msg) => Text(G.TG.major, msg);
+            internal static void Si(string msg) => Text(G.TG.si, msg);
+            internal static void Test(string msg) => Text(G.TG.test, msg);
+            internal static void IDE(string msg) => Text(G.TG.iDE, msg);
+        }
     }
 }
