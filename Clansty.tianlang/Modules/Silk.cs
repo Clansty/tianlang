81980351bb40d334a23210590a0cbdea725ed863
凌莞 <i@gao4.pw>
2020-11-15 12:23:30 +0800

    添加 silk 转码

---
+++ Clansty.tianlang/Modules/Silk.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Clansty.tianlang
+{
+    public static class Silk
+    {
+        private const string decoder = "/root/silk-v3-decoder/silk/decoder";
+        
+        public static string decode(string path)
+        {
+            Process.Start(decoder, $"{path} {path}.pcm").WaitForExit();
+            Process.Start("ffmpeg", 
+                $"-f s16le -ar 24000 -ac 1 -i {path}.pcm {path}.mp3").WaitForExit();
+            return path + ".mp3";
+        }
+    }
+}
