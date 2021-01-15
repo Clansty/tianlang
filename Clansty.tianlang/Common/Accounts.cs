147cae96fe5a7a0823cae3bcfb7f7924413db3bb
clansty <i@gao4.pw>
2021-01-15 16:54:10 +0800

    c# 8.0

--- Clansty.tianlang/Common/Accounts.cs
+++ Clansty.tianlang/Common/Accounts.cs
@@ -6,7 +6,5 @@ namespace Clansty.tianlang
     {
         public MiraiHttpSession NthsBot;
         public MiraiHttpSession Clansty;
-        public const long NTHSBOT = 1980853671;
-        public const long CLANSTY = 839827911;
     }
 }
